e76547b2cc67258d7cf403e09c40272f412aba77
Rick van Schijndel <rol3517@gmail.com>
2018-05-24 21:55:33 +0200

    First version of bot that can be subscribed to

--- bot.py
+++ bot.py
@@ -18,9 +18,7 @@ from dateutil import parser
 import canvas
 from html.parser import HTMLParser
 
-
 fhict_canvas_url = "https://fhict.instructure.com"
-
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -69,19 +67,27 @@ def retrieve_announcements_after_yesterday():
 
 
 def publish_if_new_announcements(bot, job):
-    chat_id = job.context
-    logger.info("Publishing new info to user id: {}".format(chat_id))
-    # TODO: check for new announcements, save 'now' timestamp
-    bot.send_message(chat_id=chat_id, text="hello")
-
-
-def get(bot, update):
-    logger.info("Received request for data")
-    ok, announcements_or_error = retrieve_announcements_after_yesterday()
+    chat_id = job.context["chat_id"]
+    last_retrieved = job.context["last_retrieved"]
+    
+    ok, announcements_or_error = announcement_reader.get_announcements_after(last_retrieved)
     if not ok:
-        update.message.reply_text("error: {}".format(announcements_or_error))
+        error = announcements_or_error
+        bot.send_message(chat_id=chat_id, text="error: {}".format(error))
         return
     announcements = announcements_or_error
+    if len(announcements) == 0:
+        logger.info("No new announcements")
+        return
+    formatted_announcements = get_announcements_formatted(announcements)
+    text_to_send = '\n'.join(formatted_announcements)
+    bot.send_message(chat_id=chat_id, text=text_to_send)
+
+    logger.info("Publishing new info to user id: {}".format(chat_id))
+    job.context["last_retrieved"] = datetime.datetime.now(datetime.timezone.utc) 
+
+
+def get_announcements_formatted(announcements):
     announcement_texts = []
     announcement_texts.append("Recent announcements\n")
     for announcement in announcements:
@@ -90,7 +96,20 @@ def get(bot, update):
         post_time = announcement["posted_at"]
         message = strip_tags(announcement["message"])
         announcement_texts.append("Title: {}\n\tAuthor: {}\n\tTime: {}\n\tMessage: {}\n".format(title, author, post_time, message))
-    text_to_send = '\n'.join(announcement_texts)
+    
+    return announcement_texts
+
+
+
+def get(bot, update):
+    logger.info("Received request for data")
+    ok, announcements_or_error = retrieve_announcements_after_yesterday()
+    if not ok:
+        error = announcements_or_error
+        update.message.reply_text("error: {}".format(error))
+    announcements = announcements_or_error
+    formatted_announcements = get_announcements_formatted(announcements)
+    text_to_send = '\n'.join(formatted_announcements)
     update.message.reply_text(text_to_send)
 
 
@@ -98,24 +117,35 @@ def subscribe(bot, update, job_queue, chat_data):
     # for now this is pretty naive, it would be better to have a list of chat_ids and run a job globally instead of for every subscriber
     # it should work though and that's more important
     # it will break when too much people subscribe and the total time doing requests to canvas exceeds the time slot between runs of the jobs
-
+    if "job" in chat_data:
+        update.message.reply_text("You are already subscribed.")
+        return
     chat_id = update.message.chat_id
-    time = 1
-    job = job_queue.run_once(publish_if_new_announcements, time, context=chat_id) # TODO: run every x minutes
+    second = 1
+    minute = 60 * second
+    time = 5 * minute
+    # to show all announcements after last_retrieved on first run
+    # set to now to disable showing older announcements
+    last_retrieved = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=2)
+    job = job_queue.run_repeating(callback=publish_if_new_announcements,
+            interval=time,
+            first=0,
+            context={"chat_id": chat_id, "last_retrieved": last_retrieved})
     chat_data["job"] = job
     update.message.reply_text("You are now subscribed to new announcements!")
-    logger.info("New subscriber: {}".format(chat_id))
+    logger.info("New subscriber: {}, user: {}, chat: {}"
+            .format(chat_id, update.message.from_user, update.message.chat))
 
 
-def unsubscribe(update, context):
-    if 'job' not in context.chat_data:
+def unsubscribe(bot, update, chat_data):
+    if "job" not in chat_data:
         update.message.reply_text("You were not subscribed, so unsubscribing isn't possible.")
         return
-    job = context.chat_data["job"]
+    job = chat_data["job"]
     job.schedule_removal()
-    del context.chat_data["job"]
-
-    update.message.reply_text("Succesfully unsubscribed.")
+    del chat_data["job"]
+    update.message.reply_text("Successfully unsubscribed.")
+    logger.info("Unsubscriber: {}".format(update.message.chat_id))
 
 
 def error(bot, update, error):
@@ -135,7 +165,7 @@ def main():
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("get", get))
+    #dp.add_handler(CommandHandler("get", get))
     dp.add_handler(CommandHandler("subscribe", subscribe,
                                   pass_job_queue=True,
                                   pass_chat_data=True))

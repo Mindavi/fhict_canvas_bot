747a7b43926d05f694a69aef351794cd25554b2e
Rick van Schijndel <rol3517@gmail.com>
2018-05-28 23:17:39 +0200

    Let subscriber manager return success or failure on removing

--- subscribermanager.py
+++ subscribermanager.py
@@ -33,9 +33,11 @@ class SubscriberManager:
         subscriber = str(subscriber)
         current_subscribers = self.read_subscribers()
         if subscriber not in current_subscribers:
-            return
+            return False
         current_subscribers.remove(subscriber)
         with open(self.file_name, 'w') as subscriber_file:
             subscribers_to_write = list(map(lambda x: "{}\n".format(x), current_subscribers))
             subscriber_file.writelines(subscribers_to_write)
+            return True
+        return False
 
